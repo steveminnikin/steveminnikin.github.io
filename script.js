8ec237103fa16deeeab0f96260fa5979fbe8ef8c
stephen minnikin <steveminnikin@hotmail.com>
2019-10-02 21:42:20 +0100

    updated

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 $(function() {
-    var noOfHawks;
-    var hawkCost = 1.1;
 
-    $('#hawkPicker').change(function() {
-        $('#hawkTotalCost').text(2.2);
-    });
+    setHawkTotalCost();
 
+    function setHawkTotalCost() {
+
+        var hawkCost = 1.10;
+        var totalHawkCost;
+
+        $('#hawkPicker').change(function() {
+            var noOfHawks;
+
+            noOfHawks = getNumberOfHawks();
+            totalHawkCost = calculateHawkTotalCost(noOfHawks, 1);
+
+            function getNumberOfHawks() {
+                noOfHawks = $('#hawkPicker').val();
+                return noOfHawks;
+            }
+
+            function calculateHawkTotalCost(noOfHawks, totalOptionalExtras) {
+                return (noOfHawks * hawkCost * totalOptionalExtras).toFixed(2);
+            }
+
+            $('#hawkTotalCost').text(totalHawkCost);
+
+            calculateOptionalExtras();
+
+            function calculateOptionalExtras() {
+
+                var hawkOptions = {
+                    stripes: $('#stripes').is(':checked'),
+                    coffee: $('#coffee').is(':checked'),
+                    paint: $('#paint').is(':checked'),
+                };
+
+
+
+                console.log(hawkOptions);
+            }
+        });
+
+
+    }
 
 });
